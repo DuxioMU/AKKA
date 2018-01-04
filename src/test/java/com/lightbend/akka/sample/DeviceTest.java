111fadfb1a4e7bbb045b5392d525bc21074a69c9
duxiaomu <dumu@runoob.com>
2018-01-04 18:05:39 +0800

    更新

--- src/test/java/com/lightbend/akka/sample/DeviceTest.java
+++ src/test/java/com/lightbend/akka/sample/DeviceTest.java
@@ -2,18 +2,31 @@ package com.lightbend.akka.sample;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.PoisonPill;
 import akka.testkit.javadsl.TestKit;
+import org.junit.Before;
 import org.junit.Test;
+import scala.concurrent.duration.FiniteDuration;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 
 public class DeviceTest {
-
+    ActorSystem system;
+    @Before
+    public void before(){
+        system = ActorSystem.create("tset");
+    }
     @Test
     public void testReplyWithEmptyReadingIfNoTemperatureIsKnown() {
-        ActorSystem system = ActorSystem.create("tset");
         TestKit probe = new TestKit(system);
         ActorRef deviceActor = system.actorOf(Device.props("group", "device"));
         deviceActor.tell(new Device.ReadTemperature(42L), probe.getRef());
@@ -21,4 +34,190 @@ public class DeviceTest {
         assertEquals(42L, response.requestId);
         assertEquals(Optional.empty(), response.value);
     }
+
+    @Test
+    public void testReplyWithLatestTemperatureReading(){
+        TestKit probe = new TestKit(system);
+        ActorRef deviceActor = system.actorOf(Device.props("group","device"));
+
+        deviceActor.tell(new Device.RecordTemperature(1L,24.0),probe.getRef());
+        assertEquals(1L,probe.expectMsgClass(Device.TemperatureRecorded.class).requestId);
+
+        deviceActor.tell(new Device.ReadTemperature(2L),probe.getRef());
+        Device.RespondTemperature response1 = probe.expectMsgClass(Device.RespondTemperature.class);
+        assertEquals(2L,response1.requestId);
+        assertEquals(Optional.of(24.0),response1.value);
+
+
+        deviceActor.tell(new Device.RecordTemperature(3L, 55.0), probe.getRef());
+        assertEquals(3L, probe.expectMsgClass(Device.TemperatureRecorded.class).requestId);
+
+        deviceActor.tell(new Device.ReadTemperature(4L), probe.getRef());
+        Device.RespondTemperature response2 = probe.expectMsgClass(Device.RespondTemperature.class);
+        assertEquals(4L, response2.requestId);
+        assertEquals(Optional.of(55.0), response2.value);
+    }
+
+    @Test
+    public void testReplyToRegistrationRequests() {
+        TestKit probe = new TestKit(system);
+        ActorRef deviceActor = system.actorOf(Device.props("group", "device"));
+
+        deviceActor.tell(new DeviceManager.RequestTrackDevice("group", "device"), probe.getRef());
+        probe.expectMsgClass(DeviceManager.DeviceRegistered.class);
+        assertEquals(deviceActor, probe.getLastSender());
+    }
+
+    @Test
+    public void testIgnoreWrongRegistrationRequests() {
+        TestKit probe = new TestKit(system);
+        ActorRef deviceActor = system.actorOf(Device.props("group", "device"));
+
+        deviceActor.tell(new DeviceManager.RequestTrackDevice("wrongGroup", "device"), probe.getRef());
+        probe.expectNoMsg();
+
+        deviceActor.tell(new DeviceManager.RequestTrackDevice("group", "wrongDevice"), probe.getRef());
+        probe.expectNoMsg();
+    }
+
+    /**
+     * 向设备角色添加注册支持
+     */
+    @Test
+    public void testRegisterDeviceActor() {
+        TestKit probe = new TestKit(system);
+        ActorRef groupActor = system.actorOf(DeviceGroup.props("group"));
+
+        groupActor.tell(new DeviceManager.RequestTrackDevice("group", "device1"), probe.getRef());
+        probe.expectMsgClass(DeviceManager.DeviceRegistered.class);
+        ActorRef deviceActor1 = probe.getLastSender();
+
+        groupActor.tell(new DeviceManager.RequestTrackDevice("group", "device2"), probe.getRef());
+        probe.expectMsgClass(DeviceManager.DeviceRegistered.class);
+        ActorRef deviceActor2 = probe.getLastSender();
+        assertNotEquals(deviceActor1, deviceActor2);
+
+        // Check that the device actors are working
+        deviceActor1.tell(new Device.RecordTemperature(0L, 1.0), probe.getRef());
+        assertEquals(0L, probe.expectMsgClass(Device.TemperatureRecorded.class).requestId);
+        deviceActor2.tell(new Device.RecordTemperature(1L, 2.0), probe.getRef());
+        assertEquals(1L, probe.expectMsgClass(Device.TemperatureRecorded.class).requestId);
+    }
+
+    @Test
+    public void testIgnoreRequestsForWrongGroupId() {
+        TestKit probe = new TestKit(system);
+        ActorRef groupActor = system.actorOf(DeviceGroup.props("group"));
+
+        groupActor.tell(new DeviceManager.RequestTrackDevice("wrongGroup", "device1"), probe.getRef());
+        probe.expectNoMsg();
+    }
+
+    @Test
+    public void testReturnSameActorForSameDeviceId() {
+        TestKit probe = new TestKit(system);
+        ActorRef groupActor = system.actorOf(DeviceGroup.props("group"));
+
+        groupActor.tell(new DeviceManager.RequestTrackDevice("group", "device1"), probe.getRef());
+        probe.expectMsgClass(DeviceManager.DeviceRegistered.class);
+        ActorRef deviceActor1 = probe.getLastSender();
+
+        groupActor.tell(new DeviceManager.RequestTrackDevice("group", "device1"), probe.getRef());
+        probe.expectMsgClass(DeviceManager.DeviceRegistered.class);
+        ActorRef deviceActor2 = probe.getLastSender();
+        assertEquals(deviceActor1, deviceActor2);
+    }
+    /**
+     * 跟踪组中的设备演员
+     */
+    @Test
+    public void testListActiveDevices() {
+        TestKit probe = new TestKit(system);
+        ActorRef groupActor = system.actorOf(DeviceGroup.props("group"));
+
+        groupActor.tell(new DeviceManager.RequestTrackDevice("group", "device1"), probe.getRef());
+        probe.expectMsgClass(DeviceManager.DeviceRegistered.class);
+
+        groupActor.tell(new DeviceManager.RequestTrackDevice("group", "device2"), probe.getRef());
+        probe.expectMsgClass(DeviceManager.DeviceRegistered.class);
+
+        groupActor.tell(new DeviceGroup.RequestDeviceList(0L), probe.getRef());
+        DeviceGroup.ReplyDeviceList reply = probe.expectMsgClass(DeviceGroup.ReplyDeviceList.class);
+        assertEquals(0L, reply.requestId);
+        assertEquals(Stream.of("device1", "device2").collect(Collectors.toSet()), reply.ids);
+    }
+
+    /**
+     * 跟踪组中演员
+     */
+    @Test
+    public void testListActiveDevicesAfterOneShutsDown() {
+        TestKit probe = new TestKit(system);
+        ActorRef groupActor = system.actorOf(DeviceGroup.props("group"));
+
+        groupActor.tell(new DeviceManager.RequestTrackDevice("group", "device1"), probe.getRef());
+        probe.expectMsgClass(DeviceManager.DeviceRegistered.class);
+        ActorRef toShutDown = probe.getLastSender();
+
+        groupActor.tell(new DeviceManager.RequestTrackDevice("group", "device2"), probe.getRef());
+        probe.expectMsgClass(DeviceManager.DeviceRegistered.class);
+
+        groupActor.tell(new DeviceGroup.RequestDeviceList(0L), probe.getRef());
+        DeviceGroup.ReplyDeviceList reply = probe.expectMsgClass(DeviceGroup.ReplyDeviceList.class);
+        assertEquals(0L, reply.requestId);
+        assertEquals(Stream.of("device1", "device2").collect(Collectors.toSet()), reply.ids);
+
+        probe.watch(toShutDown);
+        toShutDown.tell(PoisonPill.getInstance(), ActorRef.noSender());
+        probe.expectTerminated(toShutDown);
+
+        // using awaitAssert to retry because it might take longer for the groupActor
+        // to see the Terminated, that order is undefined
+        probe.awaitAssert(() -> {
+            groupActor.tell(new DeviceGroup.RequestDeviceList(1L), probe.getRef());
+            DeviceGroup.ReplyDeviceList r =
+                    probe.expectMsgClass(DeviceGroup.ReplyDeviceList.class);
+            assertEquals(1L, r.requestId);
+            assertEquals(Stream.of("device2").collect(Collectors.toSet()), r.ids);
+            return null;
+        });
+    }
+    /**
+     * 5、查询设备组
+     */
+    @Test
+    public void testReturnTemperatureValueForWorkingDevices() {
+        TestKit requester = new TestKit(system);
+
+        TestKit device1 = new TestKit(system);
+        TestKit device2 = new TestKit(system);
+
+        Map<ActorRef, String> actorToDeviceId = new HashMap<>();
+        actorToDeviceId.put(device1.getRef(), "device1");
+        actorToDeviceId.put(device2.getRef(), "device2");
+
+        ActorRef queryActor = system.actorOf(DeviceGroupQuery.props(
+                actorToDeviceId,
+                1L,
+                requester.getRef(),
+                new FiniteDuration(3, TimeUnit.SECONDS)));
+
+        assertEquals(0L, device1.expectMsgClass(Device.ReadTemperature.class).requestId);
+        assertEquals(0L, device2.expectMsgClass(Device.ReadTemperature.class).requestId);
+
+        queryActor.tell(new Device.RespondTemperature(0L, Optional.of(1.0)), device1.getRef());
+        queryActor.tell(new Device.RespondTemperature(0L, Optional.of(2.0)), device2.getRef());
+
+        DeviceGroup.RespondAllTemperatures response = requester.expectMsgClass(DeviceGroup.RespondAllTemperatures.class);
+        assertEquals(1L, response.requestId);
+
+        Map<String, DeviceGroup.TemperatureReading> expectedTemperatures = new HashMap<>();
+        expectedTemperatures.put("device1", new DeviceGroup.Temperature(1.0));
+        expectedTemperatures.put("device2", new DeviceGroup.Temperature(2.0));
+
+        assertEquals(expectedTemperatures, response.temperatures);
+    }
+
+
 }
+
